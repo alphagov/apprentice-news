5f0f1fc1371561347e74eafaf3c18bfb51778a0f
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-03-27 18:10:55 +0100

    Update db scripts so they'll work on Heroku

---
+++ app/database.rb
@@ -0,0 +1,30 @@
+require 'pg'
+require 'etc'
+
+module Database
+  def self.init()
+    uri = URI.parse ENV.fetch('DATABASE_URL', "postgres://#{Etc.getlogin}@localhost/apprenticenews")
+    conn = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+
+    unless submissions_table_created(conn)
+      puts 'No submissions found, creating some example ones'
+
+      conn.exec "create table submissions (id bigserial primary key, url text not null, title text not null)"
+      submissions = [
+        {url:'https://www.gov.uk', title: 'Really cool website where you can interact with the UK Government'},
+        {url:'https://duckduckgo.com', title: 'The search engine that doesn\'\'t track you'},
+        {url:'https://github.com/alphagov/apprentice-news', title: 'The most awesome link aggregation site ever'},
+      ]
+
+      submissions.each do |submission|
+        conn.exec "insert into submissions (url, title) values ('#{submission[:url]}','#{submission[:title]}')"
+      end
+    end
+  end
+
+  def self.submissions_table_created(conn)
+    conn.exec "select exists (select 1 from information_schema.tables where table_name = 'submissions')" do |result|
+      return result.any? { |row| row['exists'] == 't' }
+    end
+  end
+end
